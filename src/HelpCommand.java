fd06620b08f52c4548b34ae8ae2d2303373635a4
Mihai Maruseac <mihai.maruseac@gmail.com>
2014-12-16 20:00:42 -0500

    Cleanup Help class.

--- src/HelpCommand.java
+++ src/HelpCommand.java
@@ -78,13 +78,9 @@ public class HelpCommand extends Command {
 		};
 
 		/*
-		System.out.println("Possible shell commands:");
-		System.out.println("\texit\t\t\t\tleave shell");
-		System.out.println("\thelp\t\t\t\tshow help (this message)");
 		System.out.println("\thistory\t\t\t\tshow command history");
 		System.out.println("\tredo\t\t\t\tredo last command");
 		System.out.println("\tundo\t\t\t\ttry to undo last command");
-		System.out.println("\t----");
 		System.out.println("\tcd [<dir>]\t\t\tchange directory");
 		System.out.println("\tdir [<paths>]\t\t\tshow detailed contents of paths");
 		System.out.println("\tls [<paths>]\t\t\tshow summary contents of paths");
@@ -95,7 +91,6 @@ public class HelpCommand extends Command {
 		System.out.println("\trmdir <dir> [<dirs>]\t\tremove empty directories in current directory");
 		System.out.println("\tsort <method> <directory>\tdisplays detailed info of directory contents in sorted order");
 		System.out.println("\ttouch <file> [<files>]\t\tcreate files in current directory");
-		System.out.println("\t----");
 		System.out.println("\tchuser <user>\t\t\tchange current user");
 		System.out.println("\tchown <user> <path> [<paths>]\tchange owner of filesystem element(s)");
 		*/
